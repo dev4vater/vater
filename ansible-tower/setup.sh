a9345e7c9ce96151f46160ce6485d9a495fb7fda
Uriah Wardlaw <Uriah.wardlaw@gmail.com>
2021-09-20 13:42:49 -0500

    Added ansible setup

---
+++ ansible-tower/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Repo for netinstall
+# http://mirror.centos.org/centos/7/os/x86_64/
+
+yum -y update
+systemctl enable sshd
+systemctl status sshd
+tar -zxvf ansible-automation-platform-setup-bundle-1.2.1-1.tar.gz
+mv ./inventory ./ansible-automation-platform-setup-bundle-1.2.1-1/inventory 
+./ansible-automation-platform-setup-bundle-1.2.1-1/setup.sh
