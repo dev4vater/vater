72dd70955a28158bad5576d67315e514708efa2f
Uriah Wardlaw <uriah.wardlaw@gmail.com>
2021-09-20 14:37:41 +0000

    Added wait-for-it install

--- control-services/setup.sh
+++ control-services/setup.sh
@@ -29,9 +29,10 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install terraform
+sudo apt-get update && sudo apt-get install -Y terraform
 
-sudo apt-get update && sudo apt-get install packer
+sudo apt-get update && sudo apt-get install -y packer
+sudo apt-get install -y wait-for-it
 
 ./start.sh
 
