12d6d8cf12cea556b9ac800e3987559dc47e28dc
Uriah Wardlaw <Uriah.wardlaw@gmail.com>
2021-09-27 01:49:20 +0000

    Update

--- control-services/start.sh
+++ control-services/start.sh
@@ -8,5 +8,3 @@ sudo docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres
 sudo docker-compose down
 sudo docker system prune -f
 sudo docker-compose up -d --build --remove-orphans
-sleep 10
-wait-for-it localhost:8080 --strict -- rm ./temp -rf
