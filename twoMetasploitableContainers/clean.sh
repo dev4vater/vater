b1d1d315f691b94576a7c69b1d8d1ede8d7d3db2
uwardlaw <55300322+uwardlaw@users.noreply.github.com>
2021-10-13 08:37:37 -0500

    Delete twoMetasploitableContainers directory

--- twoMetasploitableContainers/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# If they're running, shutdown the previous containers
-sudo docker container rm twometasploitablecontainers_ms2-1_1 twometasploitablecontainers_ms2-2_1 -f
-
-# Remove network
-sudo docker network rm ms2Net
--- twoMetasploitableContainers/ms2/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Start all the exploitable services
-/bin/services.sh
-
-# Run bash to prevent the container from closing automatically
-bash
