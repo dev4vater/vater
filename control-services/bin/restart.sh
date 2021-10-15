85d44d499357d94e34ad96f0f933c16967fb0b8a
Uriah Wardlaw <uriah.wardlaw@gmail.com>
2021-10-15 20:00:16 +0000

    Added gitea configuration commands

--- control-services/bin/restart.sh
+++ control-services/bin/restart.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Ensure folders are prepared for first time deployments
-mkdir --parents ./data
-mkdir --parents ./temp
-chmod -R +x ./temp
+mkdir --parents ../data
+mkdir --parents ../temp
+chmod -R +x ../temp
 
 # Grab the initial db configuration from the guacamole image to use in the guacamole_db container
-sudo docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > ./temp/initdb.sql
+sudo docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgres > ../temp/initdb.sql
 
 # Ensure the system is clean
 sudo docker-compose down
@@ -17,8 +17,16 @@ sudo docker-compose up -d --build --remove-orphans
 
 # Preempt some known race conditions between databases and their apps
 sleep 10
-rm -r ./temp
+rm -r ../temp
 
 # Configure the management semaphore project
 
 python3 ./setupSemaphore.py
+
+# Configure gitea with mirrored ROUS repo
+
+sudo docker exec -it gitea su git bash -c "gitea admin user create --admin --username config --random-password --email a@a.a"
+
+python3 ./setupGitea.py
+
+sudo docker exec -it gitea su git bash -c "gitea admin user delete --username config"
