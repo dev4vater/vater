b442b2abf02a21dc36201b73b1c4703cf2cc0278
Uriah Wardlaw <uriah.wardlaw@gmail.com>
2021-10-28 13:03:51 +0000

    Added clean scripts for each service

--- control-services/bin/clean.sh
+++ control-services/bin/clean.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 cd ..
-sudo docker system prune -a -f
-sudo docker-compose down --rmi all
-sudo rm -rf /data
-sudo rm -rf /temp
+sudo rm -rf temp
+
+case "$1" in
+    semaphore) sudo docker-compose stop semaphore semaphore_db
+               sudo docker system prune -a -f
+               sudo rm -rf data/semaphore*
+               exit
+               ;;
+        gitea) sudo docker-compose stop gitea gitea_db
+               sudo docker system prune -a -f
+               sudo rm -rf data/gitea*
+               exit
+               ;;
+            *) echo "Cleans the data and artificats associated with a service [containerName]"
+               echo "./clean [containerName]"
+               exit
+               ;;
+esac
