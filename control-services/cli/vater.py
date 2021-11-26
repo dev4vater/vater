2ef027e4e482b353343765d8ff7009e5f65cab5b
uwardlaw <uriah.wardlaw@gmail.com>
2021-11-26 17:18:31 +0000

    Gitea login control and setup call

--- control-services/cli/vater.py
+++ control-services/cli/vater.py
@@ -1,10 +1,19 @@
 from parser import Parser
 from config import Config
+from gitea import Gitea
+import getpass
 
 def main():
     p = Parser()
     c = Config(p.args.configPath)
     p.completeParser(c.cfg["service_list"])
 
+    g = Gitea(c)
+    
+    while(not g.login(config_password=passwd)):
+        passwd = getpass.getpass(prompt="Password: ")
+
+    g.setup()    
+        
 if __name__ == "__main__":
     main()
