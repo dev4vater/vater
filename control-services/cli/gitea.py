52a6b3af9f5da19928bab74a22b86065f3fe2ea1
uwardlaw <uriah.wardlaw@gmail.com>
2021-11-27 17:16:14 +0000

    Bugs and formatting

--- control-services/cli/gitea.py
+++ control-services/cli/gitea.py
@@ -1,16 +1,17 @@
 from api import Api
-import subprocess
+from subprocess import check_output
 import json
 
 class Gitea():
     def __init__(self, configs):
         self.api = Api()
-        self.cfg = config 
+        self.cfg = configs.cfg 
 
-    def login(
-        config_password=self.cfg['gitea']['config_password']
-    ):
-        self.api.s.auth(
+    def login(self, config_password=None):
+        if config_password is None:
+            config_password=self.cfg['gitea']['config_password']
+
+        self.api.s.auth = (
             self.cfg['gitea']['config_user'],
             config_password
         )
@@ -20,36 +21,34 @@ class Gitea():
 
         # Confirm we can get a list of tokens, which requires auth
         r = self.api.s.get(url=self.cfg['gitea']['api']['tokens'])     
+
         if not r.status_code == 200:
             return false
 
-        self.__revokeConfigurationToken()
-
         # If the token exists, we have no way of getting the hash
         #   so remove the token
-        if tokenID != None:
-            url = self.cfg['gitea']['api']['tokens'] + '/' + str(tokenID)
-            self.api.delete(url=url)
+        self.__revokeConfigurationToken()
 
-        data = '{"name": "configurationToken"}'
+        # Get a new configuration token
         r = self.api.post(
-                url = self.cfg['gitea']['api']['tokens'],
-                data = data
+                url=self.cfg['gitea']['api']['tokens'],
+                data= '{"name": "configurationToken"}'
             )
 
-        tokenHash = json.loads(response.text)['sha1']
-        self.s.headers.update({'Authorization': 'Token {tokenHash})
+        # Update auth header with new token
+        tokenHash = json.loads(r.text)['sha1']
+        self.api.s.headers.update({'Authorization': 'Token {tokenHash}'})
 
-        return true
+        return True
 
     def setup(self):
         # Create organization
         self.__createOrg()
 
-        # Create repo
+        # Create content repo
         self.__createContentRepo()
 
-        # Revoke token
+        # Revokes token
         self.__revokeConfigurationToken()
 
     def syncContentRepo(self):
@@ -62,84 +61,144 @@ class Gitea():
         # Select the config user from a list of users currently
         #   available on Gitea. Returns nothing if the user
         #   does not exist
-        dockerCmd = ('gitea admin user list | grep ' +           
-                    config_user +                               
-                    ' | tr -s \' \' | cut -d \' \' -f 2')        
+        dockerCmd = (
+            'gitea admin user list | grep ' +           
+            self.cfg['gitea']['config_user'] +                               
+            ' | tr -s \' \' | cut -d \' \' -f 2'
+        )        
 
-        stream = subprocess.Popen(
-            ['sudo', 'docker', 'exec', '-it', 'gitea',
-            'su', 'git', 'bash', '-c',
-            '""' + dockerCmd + '""'
+        out = check_output(
+            [
+                'sudo', 'docker', 'exec', '-it', 'gitea',
+                'su', 'git', 'bash', '-c',
+                '""' + dockerCmd + '""'
+            ],
+            universal_newlines=True
         )
 
-        userFound = stream.read().strip()
+        userFound = out.strip()
 
-        if userFound != config_user:
+        if userFound != self.cfg['gitea']['config_user']:
             return False
-        else
+        else:
             return True
 
-    def __createConfigUser(
-        self,
-        config_password=self.cfg['gitea']['config_user']
-    ):
-        dockerCmd = ('gitea admin user create --admin'
-                    '--username ' + config_user + 
-                    '--email ' + self.cfg['gitea']['config_email'] +
-                    '--password ' + config_password + 
-                    '--must-change-password=false')
+    def __createConfigUser(self, config_password=None):
+        if config_password is None:
+            config_password=self.cfg['gitea']['config_password']
+       
+        dockerCmd = (
+            'gitea admin user create --admin'
+            '--username ' + config_user + 
+            '--email ' + self.cfg['gitea']['config_email'] +
+            '--password ' + config_password + 
+            '--must-change-password=false'
+        )
                     
-        stream = subprocess.Popen(
-            ['sudo', 'docker', 'exec', '-it', 'gitea',
-            'su', 'git', 'bash', '-c',
-            '""' + dockerCmd + '""' 
+        out = check_output(
+            [
+                'sudo', 'docker', 'exec', '-it', 'gitea',
+                'su', 'git', 'bash', '-c',
+                '""' + dockerCmd + '""'
+            ],
+            universal_newlines=True
         )
                     
    
     def __copyLatestContentRepo(self):
         # Pull from remote to local, assuming local does not have uncommitted changes
         # git --git-dir /home/control/$CONFIG_REPO_NAME/.git pull
-        stream = subprocess.Popen(
-            ['git', '--git-dir',
-            self.cfg['gitea']['content_repo_git_dir_path'], 'pull']
+        out = check_output(
+            [
+                'git', '--git-dir',
+                self.cfg['gitea']['content_repo_git_dir_path'], 'pull'
+            ],
+            universal_newlines=True
         )
 
         # Display a diff of old repo and new repo
-        stream = subprocess.Popen(
-            ['sudo', 'git', 'diff', '--diff-filter=r', '--name-status',
-            '--compact-summary', '--color', '--no-index',
-            self.cfg['gitea'['content_repo_path'],
-            self.cfg['host']['content_dir_path']]
+        out += check_output(
+            [
+                'sudo', 'git', 'diff', '--diff-filter=r', '--name-status',
+                '--compact-summary', '--color', '--no-index',
+                self.cfg['gitea']['content_repo_path'],
+                self.cfg['host']['content_dir_path']
+            ],
+            universal_newlines=True
         )
         
         # sudo rm -rf data/gitea/git/$CONFIG_REPO_NAME
-        stream = subprocess.Popen(
-            ['sudo', 'rm', '-rf', self.cfg['gitea']['content_repo_path']]
+        out += check_output(
+            [
+                'sudo', 'rm', '-rf', self.cfg['gitea']['content_repo_path']
+            ],
+            universal_newlines=True
         )
 
         # Copy repo over for gitea to import
         # sudo cp -r /home/control/$CONFIG_REPO_NAME/ data/gitea/git/$CONFIG_REPO_NAME/
-        stream = subprocess.Popen(
-            ['sudo', 'cp', '-r', 
-             self.cfg['host']['content_dir_path'],
-             self.cfg['gitea']['content_repo_path']]            
+        out += check_output(
+            [
+                'sudo', 'cp', '-r', 
+                self.cfg['host']['content_dir_path'],
+                self.cfg['gitea']['content_repo_path']
+            ],
+            universal_newlines=True
         )
 
         # Change the branch to what is expected by Semaphore
         # sudo git --git-dir data/gitea/git/$CONFIG_REPO_NAME/.git branch -m main master
-        stream = subprocess.Popen(
-            ['sudo', 'git', '--git-dir',
-            self.cfg['gitea']['content_repo_git_dir_path'],
-            'branch', '-m', 'main', 'master']
+        out += check_output(
+            [
+                'sudo', 'git', '--git-dir',
+                self.cfg['gitea']['content_repo_git_dir_path'],
+                'branch', '-m', 'main', 'master'
+            ],
+            universal_newlines=True
         )
 
-        return stream
+        return out
 
     def __createOrg(self):
-        pass
+        # Check to see if the Org exists before trying to create it
+        orgID = self.api.getIDFromName(
+            url=self.cfg['gitea']['api']['orgs'],
+            key='username', name='333TRS'
+        )
+
+        if orgID == None:
+            self.api.post(
+                url = self.cfg['gitea']['api']['tokens'],
+                data = (
+                    '{'
+                    '"username": "333TRS"'
+                    '}'
+                )
+            )
         
-    def __createContentRepo(self)
-        pass
+    def __createContentRepo(self):
+        # Check to see if the repo exists before trying to create it            universal_newlines=True
+        repoID = self.api.getIDFromName(
+            url=self.cfg['gitea']['api']['content_repo'],
+            key='name', name=self.cfg['content_repo']['name']
+        )
+
+        if repoID == None:
+            # Ensure the repository is available for migration in Gitea
+            self.__copyLatestContentRepo()
+            self.api.post(
+                url = self.cfg['gitea']['api']['repos_migrate'],
+                data = (
+                    '{' 
+                    '"clone_addr": "' + self.cfg['gitea']['container_content_repo'] + '", ' 
+                    '"private": false, '                                      
+                    '"mirror": true, '                                        
+                    '"mirror_interval": "0h0m0s", '                           
+                    '"repo_name": "' + self.cfg['content_repo']['name']  + '", '                                   
+                    '"repo_owner": "' + self.cfg['gitea']['org_or_user'] + '"'
+                    '}'
+                )
+            )
 
     # Checks for the configuration token and deletes
     #   the token if it it exists
