8aeeeb6574969fc0e2e3499217beb99e5669c499
uwardlaw <uriah.wardlaw@gmail.com>
2021-11-27 18:52:26 +0000

    Add various commands

--- control-services/cli/vater.py
+++ control-services/cli/vater.py
@@ -8,14 +8,52 @@ def main():
     c = Config(p.args.configPath)
     p.completeParser(c.cfg["service_list"])
 
-    g = Gitea(c)
+    if p.args.command == 'init':
+        init(c, p.args)
+    elif p.args.command == 'task':
+        task(c, p.args)
+    elif p.args.command == 'sync':
+        sync(c, p.args)
+    elif p.args.command == 'stop':
+        stop(c, p.args)
+    elif p.args.command == 'restart':
+        restart(c, p.args)
+    elif p.args.command == 'clean':
+        clean(c, p.args)
+    elif p.args.command == 'access':
+        access(c, p.args)
 
-    while True:
-        password = gp.getpass(prompt='Password: ')
-        if(g.login(config_password=password)):
-            break
 
-    g.setup()    
+def init(config, args):
+    pass
+
+def task(config, args):
+    pass
+
+def sync(config, args):
+    pass
+
+def stop(config, args):
+    if args.service == 'gitea' or args.service == 'gitea_db':
+
+def restart(config, args):
+    if args.service == 'gitea' or args.service == 'gitea_db':
+        g = Gitea(config)
+
+        while True:
+            password = gp.getpass(prompt='Password: ')
+            if(g.login(config_password=password)):
+                break
+
+        g.restart()
+    if args.service == 'semaphore' or args.service == 'semaphore_db':
+        return
+
+def clean(config, args):
+    if args.service == 'gitea' or args.service == 'gitea_db':
+
+def access(config, args):
+    if args.service == 'gitea' or args.service == 'gitea_db':
         
 if __name__ == "__main__":
     main()
